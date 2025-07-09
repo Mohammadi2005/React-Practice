1b78af08e26ee0de8c663f00a6cb0a1b861a27e2
amir hossein <amohammadi161183@gmail.com>
2025-07-09 10:41:51 +0330

    Timer (useEffect)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
-    const [text, setText] = useState('');
-    const [light, setLight] = useState(false);
 
+    const [second, setSecond] = useState(0);
+    const [minute, setMinute] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
 
+    useEffect(() => {
+        let timer
+        if (isRunning) {
+        timer = setInterval(() => {
+            setSecond((prevSecond) => {
+                if (prevSecond === 59) {
+                    setMinute(prevMinute => prevMinute + 0.5);
+                        return 0;
+                    }
+                    else {
+                        return prevSecond + 1;
+                    }
+                })
+            }, 1000)
+        }
+
+        return () => clearInterval(timer)
+    })
+
+    const stop = () => {
+        setIsRunning(false);
+    }
+
+    const start = () => {
+        setIsRunning(true);
+        setSecond(0);
+        setMinute(0);
+    }
+
+    const reset = () => {
+        setIsRunning(false);
+        setSecond(0);
+        setMinute(0);
+    }
 
     return (
-        <div className={light ? 'light' : 'dark'} >
-            <br/>
-            <input type="text" placeholder="Enter text" onChange={e => setText(e.target.value)}/>
-            <h1>length : {text.length}</h1><br/>
-            {text && <h1>text : {text}</h1>}
-            <button onClick={() => setLight(!light)}>{light ? 'Dark' : 'Light'}</button>
+        <div className="App" >
+            <h1>{minute} : {second}</h1>
+            <button onClick={stop}>Stop</button>
+            <button onClick={start}>Start</button>
+            <button onClick={reset}>Reset</button>
         </div>
     );
 }
