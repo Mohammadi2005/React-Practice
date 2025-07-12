6c76433161347d57a65546c0030900315243d539
amir hossein <amohammadi161183@gmail.com>
2025-07-12 09:12:38 +0330

    counter (useContext and component)

--- src/App.js
+++ src/App.js
@@ -1,37 +1,33 @@
 import './App.css';
-import {Cat} from './pages/cat'
-import {Home} from './pages/home'
-import {Dog} from './pages/dog'
-import {Horse} from './pages/horse'
-import {Error} from './pages/Error'
-import {Route, Routes, Link, BrowserRouter as Router} from 'react-router-dom'
+import {useState} from "react";
+import {createContext, useContext} from "react";
 
+const MyContext = createContext()
 
 
 function App() {
 
+    const [count, setCount] = useState(0);
+
     return (
         <div className="App" >
-            <Router>
-                <div style={{backgroundColor:'#f0c444' , fontSize: "40px", padding : "9px"}}>
-                    <Link to="/">Home</Link><br/>
-                    <Link to="/cat">Cat</Link><br/>
-                    <Link to="/dog">Dog</Link><br/>
-                    <Link to="/horse">Horse</Link>
-                </div>
-                <Routes>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/cat" element={<Cat/>}/>
-                    <Route path="/dog" element={<Dog/>}/>
-                    <Route path="/horse" element={<Horse/>}/>
-                    <Route path="*" element={<Error/>}/>
-                </Routes>
-            </Router>
+            <MyContext.Provider value={{count, setCount}}>
+                <Counter/>
+            </MyContext.Provider>
         </div>
     );
 }
 
-
-
+function Counter() {
+    const {count, setCount }= useContext(MyContext)
+    return (
+        <div className="Counter">
+            <h1>{count}</h1>
+            <button onClick={() => setCount(count + 1)}>+</button>
+            <button onClick={() => setCount(count - 1)}>-</button>
+            <button onClick={() => setCount(0)}>reset</button>
+        </div>
+    )
+}
 
 export default App;
