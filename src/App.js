cb62398f5f87ced40ca856824756647f74fd40b1
amir hossein <amohammadi161183@gmail.com>
2025-07-12 09:25:17 +0330

    get and show name

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
 import './App.css';
 import {useState} from "react";
-import {createContext, useContext} from "react";
-
-const MyContext = createContext()
 
 
 function App() {
 
-    const [count, setCount] = useState(0);
+    const [name, setName] = useState('');
+    const [showName, setShowName] = useState(false);
 
     return (
         <div className="App" >
-            <MyContext.Provider value={{count, setCount}}>
-                <Counter/>
-            </MyContext.Provider>
+            <input type="text" placeholder="Name" onChange={(e) => setName(e.target.value)}/>
+            <button onClick={() => setShowName(!showName)}>{!showName ? "show" : "hidden"}</button>
+            {showName && <h1>name : {name}</h1>}
         </div>
     );
 }
 
-function Counter() {
-    const {count, setCount }= useContext(MyContext)
-    return (
-        <div className="Counter">
-            <h1>{count}</h1>
-            <button onClick={() => setCount(count + 1)}>+</button>
-            <button onClick={() => setCount(count - 1)}>-</button>
-            <button onClick={() => setCount(0)}>reset</button>
-        </div>
-    )
-}
+
 
 export default App;
