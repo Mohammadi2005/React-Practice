d7fc9a71cc2f97743887b099735a4e4674fa1877
amir hossein <amohammadi161183@gmail.com>
2025-07-12 10:31:11 +0330

    render list

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
 import './App.css';
 import {useState} from "react";
+import axios from "axios";
 
 
 function App() {
 
-    const [name, setName] = useState('');
-    const [showName, setShowName] = useState(false);
+    const [users, setUsers] = useState([]);
+    const [names, setNames] = useState([]);
+
+    function getUsers() {
+        axios.get("https://jsonplaceholder.typicode.com/users")
+            .then((response) => {
+                setUsers(response.data);
+                console.log(response.data);
+                response.data.forEach(user => {
+                    console.log(user.name);
+                })
+            })
+    }
+
+    function Delete(id){
+        let newUsers = users.filter((user) => user.id !== id)
+        setUsers(newUsers);
+    }
 
     return (
         <div className="App" >
-            <input type="text" placeholder="Name" onChange={(e) => setName(e.target.value)}/>
-            <button onClick={() => setShowName(!showName)}>{!showName ? "show" : "hidden"}</button>
-            {showName && <h1>name : {name}</h1>}
+            <button onClick={getUsers} >Get Started</button>
+            <ShowName users={users} Delete={Delete} />
         </div>
     );
 }
 
-
+function ShowName(props) {
+    return (
+        <div className="App">
+            {props.users.map((user) => {
+                return (
+                    <div key={`div-${user.id}`}>
+                        <h3>{user.id} - {user.name}</h3>
+                        <button onClick={() => props.Delete(user.id)}>Delete</button>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
 
 export default App;
