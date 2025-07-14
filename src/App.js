b8d1f94fa50f2f4aff08eca9e6761db5a92b9f45
amir hossein <amohammadi161183@gmail.com>
2025-07-14 17:29:35 +0330

    change Theme with useContext

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import './App.css';
-import {Person} from "./propTypes/Person";
+import {createContext, useContext, useState} from 'react'
+
+const ThemeContext = createContext()
 
 
 function App() {
 
+    const [themeColor, setThemeColor] = useState("#326533")
+
     return (
-        <div className="App" >
-            <Person name={"ali"} email={"ali@gmail.com"} age={20} isMarried={false} courses={["C", "C++"]} />
+        <div className="App"  style={{width: '100%', height: '100%'}}>
+            <ThemeContext.Provider value={{themeColor, setThemeColor}}>
+                <MyPage/>
+            </ThemeContext.Provider>
         </div>
     );
+}
+
+function MyPage() {
+    const {themeColor, setThemeColor} = useContext(ThemeContext)
+    return (
+        <div className="App" style={{backgroundColor: themeColor, padding: "20px", height: '549px'}}>
+            <h1>my page</h1>
+            <p>you can written your favorite color code in box </p>
+            <input onChange={(e) => setThemeColor(`#${e.target.value}`)} />
+        </div>
+    )
 }
 
 export default App;
