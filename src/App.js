3c641b520025fde2c244bdd07c504e30ef17b48c
amir hossein <amohammadi161183@gmail.com>
2025-07-14 16:55:22 +0330

    propTypes

--- src/App.js
+++ src/App.js
@@ -1,50 +1,14 @@
 import './App.css';
-import {useState} from "react";
-import axios from "axios";
+import {Person} from "./propTypes/Person";
 
 
 function App() {
 
-    const [users, setUsers] = useState([]);
-    const [names, setNames] = useState([]);
-
-    function getUsers() {
-        axios.get("https://jsonplaceholder.typicode.com/users")
-            .then((response) => {
-                setUsers(response.data);
-                console.log(response.data);
-                response.data.forEach(user => {
-                    console.log(user.name);
-                })
-            })
-    }
-
-    function Delete(id){
-        let newUsers = users.filter((user) => user.id !== id)
-        setUsers(newUsers);
-    }
-
     return (
         <div className="App" >
-            <button onClick={getUsers} >Get Started</button>
-            <ShowName users={users} Delete={Delete} />
+            <Person name={"ali"} email={"ali@gmail.com"} age={20} isMarried={false} courses={["C", "C++"]} />
         </div>
     );
-}
-
-function ShowName(props) {
-    return (
-        <div className="App">
-            {props.users.map((user) => {
-                return (
-                    <div key={`div-${user.id}`}>
-                        <h3>{user.id} - {user.name}</h3>
-                        <button onClick={() => props.Delete(user.id)}>Delete</button>
-                    </div>
-                )
-            })}
-        </div>
-    )
 }
 
 export default App;
